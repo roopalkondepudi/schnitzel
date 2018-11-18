0470db5a6945cfae4280334a72f04f2797e35c32
Roopal Kondepudi <roopal.kondepudi@gmail.com>
2018-11-17 20:41:34 -0800

    i actually give up.

--- src/schnitzel_web/FirebaseService.java
+++ src/schnitzel_web/FirebaseService.java
@@ -1,59 +1,17 @@
 package schnitzel_web;
 
-<<<<<<< HEAD
-import javax.ws.rs.*;
-=======
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 import javax.ws.rs.*;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-
->>>>>>> 1a2efaaee9e177c5fe442cc5fd2f81e02be6e0b9
 @Path("/FirebaseService") 
 
 public class FirebaseService
 {  
-<<<<<<< HEAD
 	@GET @Path("/firebase")@Produces("text/plain")
 	public String initializeFirebase()
 	{
 		return "in firebase initialization.";
 	}
-=======
-	@POST @Path("/firebase") @Produces("text/plain")
-	public String initializeFirebase()
-	{
-		FirebaseOptions options;		
-		try
-		{
-			FileInputStream serviceAccount = new FileInputStream("schnitzeljagd-9a293-firebase-adminsdk-t2x2r-b2534d086e.json");
-		
-			options = new FirebaseOptions.Builder()
-				    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				    .setDatabaseUrl("https://schnitzeljagd-9a293.firebaseio.com/")
-				    .build();
-				FirebaseApp.initializeApp(options);
-			return "firebase initialization success!";
-		}
-		catch (FileNotFoundException ex)
-		{
-			System.out.println("fnfe thrown");
-			ex.printStackTrace();
-		}
-		catch (IOException e)
-		{
-			System.out.println("ioe thrown");
-			e.printStackTrace();
-		}		
-		return "firebase initialization failure";
-	}
-	
->>>>>>> 1a2efaaee9e177c5fe442cc5fd2f81e02be6e0b9
+
 	@GET @Path("/score/wins")@Produces("text/plain")
 	public int getWins() {return Score.WINS;}
 	     
@@ -78,11 +36,7 @@ public class FirebaseService
 	public String getScore() {
 	   String pattern = 
 	      "{ \"wins\":\"%s\", \"losses\":\"%s\", \"ties\": \"%s\"}";
-<<<<<<< HEAD
 	   return String.format(pattern,  Score.WINS, Score.LOSSES, Score.TIES );   
-=======
-	   return String.format(pattern,  Score.WINS, Score.LOSSES, Score.TIES );
->>>>>>> 1a2efaaee9e177c5fe442cc5fd2f81e02be6e0b9
 	}
 	 
 	@PUT
